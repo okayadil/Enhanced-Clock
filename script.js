cc444735998f965bab4f8d10af921324ecb70700
ammaradil20 <ammaradil2020@gmail.com>
2025-03-03 15:38:12 +0530

    firstCommit

---
+++ script.js
@@ -0,0 +1,38 @@
+window.onload = () => {
+    const analogClock = document.getElementById("analog-clock");
+    const digitalClock = document.getElementById("digital-clock");
+    const dateElement = document.getElementById("date");
+    
+    // logical function of Analog Clock
+    setInterval( () => {
+        const analog = new Date ();
+        const second = analog.getSeconds() * 6;
+        const minute = analog.getMinutes() * 6;
+        const hour = analog.getHours() * 30 + Math.round(minute / 12);
+
+        document.querySelector(".second-hand-container").style.transform = "rotate(" + second + "deg)";
+        document.querySelector(".minute-hand-container").style.transform = "rotate(" + minute + "deg)";
+        document.querySelector(".hour-hand-container").style.transform = "rotate(" + hour + "deg)";
+   
+    }, 1000);
+
+    // Digital Clock
+    setInterval( () => { 
+        const updateClock = new Date ();
+        const hours = updateClock.getHours();
+        const minutes = updateClock.getMinutes();
+        const seconds = updateClock.getSeconds();
+
+    // Date 
+        const options = {
+            weekday : "long",
+            day : "numeric",
+            month : "long",
+            year : "numeric",
+        };    
+      
+        document.getElementById("digital-clock").innerHTML = `${hours < 10 ? `0${hours % 12 || 12}` : hours % 12 || 12} : ${minutes < 10 ? `0${minutes}` : minutes} : ${seconds < 10 ? `0${seconds}` : seconds}`;
+        document.getElementById("date").innerHTML = updateClock.toLocaleDateString(undefined, options)
+ 
+    }, 1000);
+};
